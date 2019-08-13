3791728167718fabc7dcf766b904b859a3c8eb9c
Ilya Hamisoniya <Ildoradogam@gmail.com>
2019-08-13 22:16:08 +0300

    Updates

---
+++ precache-manifest.12f9e273b4e7529b88b94452613907c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e018cd509091eb811b3804d23aa5044d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28201f0bab9993d2717a",
+    "url": "/static/css/main.53cc216a.chunk.css"
+  },
+  {
+    "revision": "be46bff883b825acabcc",
+    "url": "/static/js/2.d6851415.chunk.js"
+  },
+  {
+    "revision": "28201f0bab9993d2717a",
+    "url": "/static/js/main.7352ac64.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
