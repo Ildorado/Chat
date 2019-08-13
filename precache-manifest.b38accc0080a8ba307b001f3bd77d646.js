fabab8ed0217b1337d6cfaa26531a6d76a326514
Ilya Hamisoniya <Ildoradogam@gmail.com>
2019-08-13 22:32:51 +0300

    Updates

---
+++ precache-manifest.b38accc0080a8ba307b001f3bd77d646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c339f617303442699c4cc3136255a90",
+    "url": "/chatRS/index.html"
+  },
+  {
+    "revision": "b8b7028b9e0ad1f4ba3e",
+    "url": "/chatRS/static/css/main.53cc216a.chunk.css"
+  },
+  {
+    "revision": "be46bff883b825acabcc",
+    "url": "/chatRS/static/js/2.d6851415.chunk.js"
+  },
+  {
+    "revision": "b8b7028b9e0ad1f4ba3e",
+    "url": "/chatRS/static/js/main.76dec39f.chunk.js"
+  },
+  {
+    "revision": "dda1580116ce7e0c6690",
+    "url": "/chatRS/static/js/runtime~main.1a8b3fda.js"
+  }
+]);
